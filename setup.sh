71914db63bef025180effd7759f57e13966bf962
NilsVreman <nils.vreman@gmail.com>
2020-06-17 10:30:37 +0200

    Finished setup script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
 ## Paths
-BASEDIR=~/dotfiles
-SNIPPETPATH=${BASEDIR}/my-snippets
-SNIPPETTARGETPATH=~/.vim/bundle/vim-snippets/UltiSnips
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SNIPPETSOURCE=${BASEDIR}/my-snippets
+SNIPPETTARGET=~/.vim/bundle/vim-snippets/UltiSnips
+
+## Update submodules
+git submodule update --init --recursive
 
 ## Symlinks
 # Vim
 ln -sT ${BASEDIR}/vimrc ~/.vimrc
 ln -sT ${BASEDIR}/vim/ ~/.vim
 
+# Bash
+ln -sT ${BASEDIR}/bashrc ~/.bashrc
+ln -sT ${BASEDIR}/bash_profile ~/.bash_profile
+
+# Tmux
+ln -sT ${BASEDIR}/tmux.conf ~/.tmux.conf
+
+# Git
+ln -sT ${BASEDIR}/gitconfig ~/.gitconfig
+
+# SSH
+mkdir -p ~/.ssh
+ln -sT ${BASEDIR}/sshconfig ~/.ssh/config
+
 ## Plugin installation for vim
 vim +PluginInstall +qall &>/dev/null
 
-## Move my-snippets to correct place
-cp ${SNIPPETPATH}/* ${SNIPPETTARGETPATH}/
+## symlink my-snippets to correct place
+# Remove all symlinks in target dir
+for TFILE in ${SNIPPETTARGET}/*.snippets; do
+    if [[ -L ${TFILE} ]]; then
+        unlink ${TFILE}
+        rm -rf ${TFILE}
+    fi
+done
+# Add new symlinks
+for SFILE in ${SNIPPETSOURCE}/*.snippets; do
+    ln -sT ${SFILE} "${SNIPPETTARGET}/$(basename ${SFILE} .snippets).snippets"
+done
