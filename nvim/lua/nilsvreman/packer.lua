4e40e06fdba95dee9b73cf6a8c55ee783a1e6913
NilsVreman <nils.vreman@gmail.com>
2023-12-29 16:16:16 +0100

    Changed version of two major packages

--- nvim/lua/nilsvreman/packer.lua
+++ nvim/lua/nilsvreman/packer.lua
@@ -8,8 +8,7 @@ return require('packer').startup(function(use)
     use('wbthomason/packer.nvim')
 
     use({
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4', -- or; branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     })
 
@@ -19,22 +18,22 @@ return require('packer').startup(function(use)
 
     use({
         'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
+        branch = 'v3.x',
         requires = {
             -- LSP Support
             {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            run = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end,
-        },
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            {
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'},
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
         }
     })
 
