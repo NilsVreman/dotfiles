c929409ab4b690d0137c39680136c6bd8c3672fb
NilsVreman <nils.vreman@gmail.com>
2023-12-29 16:16:44 +0100

    Updated plugin to work with new major version of lsp-zero

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,47 +1,48 @@
-local lsp = require('lsp-zero').preset({})
+local lsp_zero = require('lsp-zero')
 
--- Language servers
-lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'rust_analyzer',
-	'julials',
-	'pyright',
-	'lua_ls',
-})
-
--- Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<CR>'] = cmp.mapping.confirm({ select = false }),     -- select=false to confirm if selected
-	['<C-Space>'] = cmp.mapping.complete(),
-})
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
-})
-
-lsp.set_preferences({ })
-
-lsp.on_attach(function(_, bufnr)
+lsp_zero.on_attach(function(_, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>j", function() vim.diagnostic.goto_next() end, opts) -- next err
     vim.keymap.set("n", "<leader>k", function() vim.diagnostic.goto_prev() end, opts) -- prev err
-    vim.keymap.set("n", "<leader>vc", function() vim.lsp.buf.code_action() end, opts) -- codeaction
-    vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.references() end, opts)  -- ref to thi
-    vim.keymap.set("n", "<leader>vn", function() vim.lsp.buf.rename() end, opts)      -- rename ref
+    vim.keymap.set("n", "<leader>ha", function() vim.lsp.buf.code_action() end, opts) -- codeaction
+    vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)  -- ref to thi
+    vim.keymap.set("n", "<leader>ra", function() vim.lsp.buf.rename() end, opts)      -- rename ref
 end)
 
-lsp.setup()
+-- Language servers
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'tsserver',
+        'eslint',
+        'rust_analyzer',
+        'julials',
+        'pyright',
+        'lua_ls',
+    },
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function() -- Configure lua language server for neovim
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+    }
+})
+
+local cmp = require('cmp')
+local cmp_format = lsp_zero.cmp_format()
+cmp.setup({
+    formatting = cmp_format,
+    mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),     -- select=false to confirm if selected
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['Tab'] = nil,
+        ['<S-Tab>'] = nil,
+    })
+})
 
 vim.diagnostic.config({
     underline = false,
