18843faef1b9e0122425eb147a754a6d97ad4728
NilsVreman <nils.vreman@gmail.com>
2024-01-07 17:00:18 +0100

    Added copilot to dependency list

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
