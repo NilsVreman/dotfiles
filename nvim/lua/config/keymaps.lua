0a0938aff4a8a58e0345f1ef2db8bbca579ab8ef
NilsVreman <nils.vreman@gmail.com>
2024-01-31 19:41:36 +0100

    Removed underline from lsp config on errors and similar diagnostics. Also added centering on half-page vertical movement

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,6 +4,9 @@
 --
 local Util = require("lazyvim.util")
 
+-- Deletes
+vim.keymap.del("n", "<leader>ft")
+
 -- Formatting
 vim.keymap.set({ "n", "v" }, "==", function()
   Util.format({ force = true })
@@ -20,6 +23,11 @@ end, { desc = "Terminal (cwd)" })
 -- Window splitting
 vim.keymap.set("n", "<leader>sj", "<C-W>s", { desc = "Split window below", remap = true })
 vim.keymap.set("n", "<leader>sl", "<C-W>v", { desc = "Split window right", remap = true })
+vim.keymap.set("n", "<leader>wj", "<C-W>s", { desc = "Split window below", remap = true })
+vim.keymap.set("n", "<leader>wl", "<C-W>v", { desc = "Split window right", remap = true })
 
--- deletes
-vim.keymap.del("n", "<leader>ft")
+-- Vertical movement
+vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz", { desc = "Scroll down 1/2 page", remap = true })
+vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz", { desc = "Scroll up 1/2 page", remap = true })
+vim.keymap.set("n", "<leader>o", "m`o<Esc>``")
+vim.keymap.set("n", "<leader>O", "m`O<Esc>``")
