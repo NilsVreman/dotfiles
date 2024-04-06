e6c24021de38f7b65036c54b001ef7a6f4479ba5
NilsVreman <nils.vreman@gmail.com>
2024-04-06 15:15:45 +0200

    Removed unnecessary setup commands and added backup creation

--- setup.sh
+++ setup.sh
@@ -6,8 +6,11 @@ echo "Initializing parameters and updating submodules..."
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TMUXDIR="$BASEDIR/tmux"
 NVIMDIR="$BASEDIR/nvim"
-CODEDIR="$BASEDIR/vscode"
 CONFDIR="$HOME/.config"
+BAKDIR="$HOME/backups"
+
+mkdir -p $CONFDIR
+mkdir -p $BAKDIR
 
 ## Update Submodules
 git submodule update --init --recursive
@@ -17,52 +20,42 @@ echo "Generating symbolic links..."
 
 # Bash
 if [ -e "$HOME/.bashrc" ]; then
+	cp -r $HOME/.bashrc $BAKDIR/bashrc.bak
 	rm $HOME/.bashrc # Cleaning up old links
 fi
 ln -sT $BASEDIR/bashrc $HOME/.bashrc
 source $HOME/.bashrc
 if [ -e "$HOME/.bash_profile" ]; then
+	cp -r $HOME/.bash_profile $BAKDIR/bash_profile.bak
 	rm $HOME/.bash_profile # Cleaning up old links
 fi
 ln -sT $BASEDIR/bash_profile $HOME/.bash_profile
 
 # NeoVim
 if [ -d "$CONFDIR/nvim" ]; then
+	cp -r $CONFDIR/nvim $BAKDIR/nvim.bak
 	rm $CONFDIR/nvim # Cleaning up old links
 fi
 ln -sT $NVIMDIR/ $CONFDIR/nvim
-# 1. Install nvim
-# 3. Open nvim and execute `<leader>l` followed by `I`
-# Note: if the fons aren't loading properly check this reddit thread:
-# https://www.reddit.com/r/Fedora/comments/u2fmwm/font_rendering_isnt_good_as_win_11/
+echo "Don't forget to install nvim"
+echo "NOTE: Mason (LSP/Linters/Formatters) need npm installed."
+echo "  - Also Note that both rg (ripgrep) and fd (fd-find) needs to be installed"
+echo "Open nvim and execute leader l followed by I"
 
 # Tmux
 if [ -d "$CONFDIR/tmux" ]; then
+	cp -r $CONFDIR/tmux $BAKDIR/tmux.bak
 	rm $CONFDIR/tmux # Cleaning up old links
 fi
 ln -sT $TMUXDIR $CONFDIR/tmux
 tmux source $CONFDIR/tmux/tmux.conf
-# 1. Install tpm by running:
-#   git clone https://github.com/tmux-plugins/tpm $TMUXDIR/plugins/tpm
-# 2. Open a tmux session and press `prefix + I` (capital i, as in Install) to fetch the plugins.
-
-# Julia
-mkdir -p $HOME/.julia/config
-rm $HOME/.julia/config/startup.jl
-ln -sT $BASEDIR/startup.jl $HOME/.julia/config/startup.jl
+echo "Don't forget to install tpm by running:"
+echo "  git clone https://github.com/tmux-plugins/tpm $TMUXDIR/plugins/tpm"
+echo "Open a tmux session and press 'prefix + I' (capital i, as in Install) to fetch the plugins."
 
 # Git
+cp -r $HOME/.gitconfig $BAKDIR/gitconfig.bak
 rm $HOME/.gitconfig
 ln -sT $BASEDIR/gitconfig $HOME/.gitconfig
-
-# VsCode
-if [ -e "$CONFDIR/Code/User/settings.json" ]; then
-	rm $CONFDIR/Code/User/settings.json # Cleaning up old links
-fi
-ln -sT $CODEDIR/settings.json $CONFDIR/Code/User/settings.json
-if [ -e "$CONFDIR/Code/User/keybindings.json" ]; then
-	rm $CONFDIR/Code/User/keybindings.json # Cleaning up old links
-fi
-ln -sT $CODEDIR/keybindings.json $CONFDIR/Code/User/keybindings.json
 
 echo "Done!"
