d661e9c9614752612245cd12f97038816a640249
NilsVreman <nils.vreman@gmail.com>
2025-08-29 18:07:34 +0200

    feat(nvim): borders around completions and popups

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,19 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      documentation = {
+        auto_show = false,
+        window = {
+          border = "rounded",
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+        },
+      },
+      menu = {
+        border = "rounded",
+        draw = { gap = 2 },
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+      },
+    },
+  },
+}
